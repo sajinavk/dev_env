5d3cb9aa22b6d7c2dd99c78fa4e4e464a235d390
Michael Woodcock <51295010+MichaelWoodc@users.noreply.github.com>
2025-11-18 17:52:12 -0500

    Change zsh start to system reboot
    Replaced immediate zsh start with a system reboot after a 5-second delay.

--- env_setup.sh
+++ env_setup.sh
@@ -68,5 +68,9 @@ echo
 echo "-------------------------------------------------------"
 echo " Done! Logout/login or run:  zsh "
 echo "-------------------------------------------------------"
-# Start zsh immediately for the invoking user
-sudo -u "$SUDO_USER" zsh
+echo
+echo "-------------------------------------------------------"
+echo " Done! System will reboot in 5 seconds... "
+echo "-------------------------------------------------------"
+sleep 5
+reboot
